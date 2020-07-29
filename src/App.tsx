d3c73b9dea1428b7d528919b03138e79b1147f8c
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-07-29 08:55:39 -0300

    Initial Config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+	return (
+		<>
+			<h1>Rodrigo</h1>
+			<GlobalStyles/>
+		</>
+	);
+}
+
+export default App;
