9c28636c6b069cd99cf1c95b5b47137bfe3c5902
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-08-04 18:47:07 -0300

    Create index.tsx

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const ProfilePage: React.FC = () => {
+	return (
+		<S.Container>
+			<S.Banner>
+				<S.Avatar />
+				<S.ProfileData>
+				{/* <S.EditButton outlined>Editar perfil</S.EditButton> */}
+
+					<h1>Rodrigo Barros</h1>
+					<h2>@rodrigobarros2</h2>
+
+					<p>Developer at Rodrigo Barros</p>
+
+					<ul>
+						<li>
+							<S.LocationIcon />
+							Canto do Buriti Piauí
+						</li>
+						<li>
+							<S.CakeIcon />
+							Nascido(a) em 14 de junho de 1996
+						</li>
+					</ul>
+
+					<S.Followage>
+						<span>
+							seguindo <strong>94</strong>
+						</span>
+						<span>
+							<strong>682</strong> seguidores
+						</span>
+					</S.Followage>
+
+				</S.ProfileData>
+			</S.Banner>
+		</S.Container>
+	);
+}
+
+export default ProfilePage;
