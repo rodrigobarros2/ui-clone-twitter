faf0a84fbf1f38b6d76551bd9b0f0852712c77fc
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-08-04 18:47:03 -0300

    Create index.tsx

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ProfilePage from '../ProfilePage'
+
+import * as S from './styles';
+
+const Layout: React.FC = () => {
+	return (
+		<S.Container>
+			<S.Header>
+				<button>
+					<S.BackIcon />
+				</button>
+				<S.ProfileInfo>
+					<strong>Guilerme Rodz</strong>
+					<span>612 Tweets</span>
+				</S.ProfileInfo>
+			</S.Header>
+
+			<ProfilePage />
+
+			<S.BottomMenu>
+				<S.HomeIcon className="active" />
+				<S.SearchIcons />
+				<S.BellIcons />
+				<S.EmailIcon />
+			</S.BottomMenu>
+		</S.Container>
+
+	);
+}
+
+export default Layout;
