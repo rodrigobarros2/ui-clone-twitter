3f78905e7ea173bbbd4600d94863c9cc187d1e7e
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-08-04 21:02:59 -0300

    Update styles.ts

--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -1,17 +1,78 @@
-import styled, {css} from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { LocationOn, Cake } from '../../styles/Icons'
 
 export const Container = styled.div`
-
+	display: flex;
+	flex-direction: column;
+	max-height: 100%;
+	overflow-y: auto;
+	scrollbar-width: none; /** Firefox */
+	::-webkit-scrollbar {
+	display: none;
+	}
 `;
 
 export const Banner = styled.div`
-
+	flex-shrink: 0;
+	width: 100%;
+	height: 199px;
+	background: var(--twitter);
+	position: relative;
 `;
 
 export const Avatar = styled.div`
+	width: max(45px, min(135px, 22vw));
+	height: max(45px, min(135px, 22vw));
+	border: 3.75px solid var(--primary);
+	background: var(--gray);
+	border-radius: 50%;
+	position: absolute;
+	bottom: max(-60px, -10vw);
+	left: 15px;
+	z-index:3;
+`;
 
+export const ProfileData = styled.div`
+	padding: min(calc(10vw + 7px), 67px) 16px 0;
+	display:flex;
+	flex-direction: column;
+	position:relative;
+
+	> h1 {
+		font-weight: bold;
+	}
+
+	> h2 {
+		font-weight: normal;
+		font-size: 15px;
+		color: var(--gray);
+	}
+
+	> p {
+		font-size: 15px;
+		margin-top: 11px;
+	}
+
+	> a {
+		text-decoration: none;
+		color: var(--twitter);
+	}
+
+	> ul {
+    list-style: none;
+	margin: 10px 0;
+
+		> li {
+		font-size: 15px;
+		color: var(--gray);
+
+			> svg {
+				fill: var(--gray);
+				margin-right: 5px;
+			}
+		}
+	}
 `;
 
 export const EditButton = styled.div`
@@ -20,9 +81,7 @@ export const EditButton = styled.div`
 
 export const Followage = styled.div`
 
-`;
 
-export const ProfileData = styled.div`
 
 `;
 
