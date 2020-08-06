91a06b2d0f3863f21047df4c772f54c370405d9c
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-08-05 23:24:56 -0300

    Create styles.ts

---
+++ src/components/Tweet/styles.ts
@@ -0,0 +1,114 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+`;
+
+export const Retweeted = styled.div`
+
+`;
+
+export const RocketseatIcon = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Avatar = styled.div`
+
+`;
+
+export const Header = styled.div`
+
+`;
+
+export const Dot = styled.div`
+
+`;
+
+export const ImageContent = styled.div`
+
+`;
+
+export const Icons = styled.div`
+
+`;
+
+export const CommentIcon = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
+
